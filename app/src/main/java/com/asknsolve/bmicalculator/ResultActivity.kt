008722e384829f93cd529d74517a3039ee102ff3
cava <cava@kaist.ac.kr>
2021-08-13 22:41:00 +0900

    bmi 계산 위해 !!. 사용

--- app/src/main/java/com/asknsolve/bmicalculator/ResultActivity.kt
+++ app/src/main/java/com/asknsolve/bmicalculator/ResultActivity.kt
@@ -13,16 +13,17 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        //전달받은 키와 몸무게
-        val height = intent.getStringExtra("height")?.toInt()
-        val weight = intent.getStringExtra("weight")?.toInt()
+        // 전달받은 키와 몸무게
+        // BMI 계산 위해 !!. 사용
+        val height = intent.getStringExtra("height")!!.toInt()
+        val weight = intent.getStringExtra("weight")!!.toInt()
 
         // error 체크, MainActivity 문제였음
         // Log.d("resultActivity", "resultHeight = $height , resultWeight = $weight" )
 
         // BMI 계산
-//        val bmi = weight / Math.pow(height / 100.0, 2.0)
+        val bmi = weight / Math.pow(height / 100.0, 2.0)
 
-//        Log.d("bmi", "bmi = $bmi")
+        Log.d("bmi", "bmi = $bmi")
     }
 }
